ebdfd005d17373a35447cc8227e7e963170a4aaa
Yohan <yohan.page87@gmail.com>
2024-10-04 09:33:50 +0200

    Admin Crud controllers

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -7,6 +7,11 @@ use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Classe;
+use App\Entity\User;
+use App\Entity\Place;
+use App\Entity\Event;
+use App\Entity\Student;
 
 class DashboardController extends AbstractDashboardController
 {
@@ -41,6 +46,10 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+        yield MenuItem::linkToCrud('Classes', 'fa fa-graduation-cap', Classe::class);
+        yield MenuItem::linkToCrud('Users', 'fa fa-user', User::class);
+        yield MenuItem::linkToCrud('Events', 'fa fa-calendar', Event::class);
+        yield MenuItem::linkToCrud('Places', 'fa fa-map-marker', Place::class);
+        yield MenuItem::linkToCrud('Students', 'fa fa-user-graduate', Student::class);
     }
 }
